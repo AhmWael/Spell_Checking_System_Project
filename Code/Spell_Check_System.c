e70858ffa1dac53160cfed0b965ce276ff95b6be
Ahmad Wael <ahmadwael2004@gmail.com>
2024-05-21 01:18:15 +0300

    Removed AVL tree implementation

--- Code/Spell_Check_System.c
+++ Code/Spell_Check_System.c
@@ -3,28 +3,21 @@
 #include <math.h>
 #include <string.h>
 
-#define max(a, b) (((a) > (b)) ? (a) : (b))
-
 typedef struct Node
 {
     char *data;
     struct Node *left, *right;
-    int height;
 } Node;
 
 Node *newNode(char *data);
 Node *insertNode(Node *node, char *data);
-Node *rightRotate(Node *y);
-Node *leftRotate(Node *x);
-int findHeight(Node *root);
-int getBalance(Node *N);
-void printinOrder(Node *root);
-Node *load();
 Node *findMin(Node *root);
 Node *findMax(Node *root);
 Node *search(Node *root, char *key);
 Node *getpredecessor(Node *root, Node *current);
 Node *getsuccessor(Node *root, Node *current);
+Node *load();
+int findHeight(Node *root);
 
 Node *newNode(char *data)
 {
@@ -32,7 +25,6 @@ Node *newNode(char *data)
     n->data = malloc(strlen(data) + 1);
     strcpy(n->data, data);
     n->left = n->right = NULL;
-    n->height = 1;
     return n;
 }
 
@@ -45,86 +37,7 @@ Node *insertNode(Node *node, char *data)
         node->left = insertNode(node->left, data);
     else if (strcasecmp(data, node->data) > 0)
         node->right = insertNode(node->right, data);
-
-    node->height = 1 + max(findHeight(node->left), findHeight(node->right));
-
-    int balance = getBalance(node);
-
-    // AVL Cases
-    // Left Left Case
-    if (balance > 1 && strcasecmp(data, node->left->data) < 0)
-        return rightRotate(node);
-    // Right Right Case
-    if (balance < -1 && strcasecmp(data, node->right->data) > 0)
-        return leftRotate(node);
-    // Left Right Case
-    if (balance > 1 && strcasecmp(data, node->left->data) > 0)
-    {
-        node->left = leftRotate(node->left);
-        return rightRotate(node);
-    }
-    // Right Left Case
-    if (balance < -1 && strcasecmp(data, node->right->data) < 0)
-    {
-        node->right = rightRotate(node->right);
-        return leftRotate(node);
-    }
-
     return node;
-}
-
-Node *rightRotate(Node *y)
-{
-    // printf("right rotate\n");
-    Node *x = y->left;
-    Node *Temp = x->right;
-    // Perform rotation
-    x->right = y;
-    y->left = Temp;
-    // Update heights
-    y->height = max(findHeight(y->left), findHeight(y->right)) + 1;
-    x->height = max(findHeight(x->left), findHeight(x->right)) + 1;
-    // Return new root
-    return x;
-}
-
-Node *leftRotate(Node *x)
-{
-    // printf("left rotate\n");
-    Node *y = x->right;
-    Node *Temp = y->left;
-    // Perform rotation
-    y->left = x;
-    x->right = Temp;
-    // Update heights
-    x->height = max(findHeight(x->left), findHeight(x->right)) + 1;
-    y->height = max(findHeight(y->left), findHeight(y->right)) + 1;
-    // Return new root
-    return y;
-}
-
-int findHeight(Node *root)
-{
-    if (root == NULL)
-        return 0;
-    return root->height;
-}
-
-int getBalance(Node *N)
-{
-    if (N == NULL)
-        return 0;
-    return findHeight(N->left) - findHeight(N->right);
-}
-
-void printinOrder(Node *root)
-{
-    if (root != NULL)
-    {
-        printinOrder(root->left);
-        printf("%s (%d)", root->data, root->height);
-        printinOrder(root->right);
-    }
 }
 
 Node *load()
@@ -151,8 +64,13 @@ Node *load()
         // printf("Record: \"%s\"", record);
         root = insertNode(root, record);
     }
-    printf("Number of words: %d\n", num_word);
-    printf("Height: %d (used AVL Tree)\n", findHeight(root));
+    printf("..................................\n");
+    printf("Dictionary Loaded Successfully...!\n");
+    printf("..................................\n");
+    printf("Size = %d\n", num_word);
+    printf("..................................\n");
+    printf("Height = %d\n", findHeight(root));
+    printf("..................................\n");
     rewind(fp);
     fclose(fp);
     return root;
@@ -176,6 +94,13 @@ Node *findMax(Node *root)
         return root;
     else
         return findMax(root->right);
+}
+
+int findHeight(Node *root)
+{
+    if(root == NULL)
+        return -1;
+    return 1 + fmax(findHeight(root->left), findHeight(root->right));
 }
 
 Node *search(Node *root, char *key)
@@ -246,7 +171,6 @@ Node *getsuccessor(Node *root, Node *current)
 void main()
 {
     Node *root = load();
-    //printinOrder(root);
     char *sentence = malloc(80);
     printf("\nEnter a sentence: ");
     fgets(sentence, 80, stdin);
@@ -255,7 +179,6 @@ void main()
     while (token)
     {
         Node *temp = search(root, token);
-        
         if (!strcasecmp(token, temp->data))
             printf("%s - CORRECT\n", token);
         else
